eec768ee0f89c49d7eefccee084f34fa91bc278b
Ilya Sumbatyants <ilya.sumb@gmail.com>
2014-02-07 10:16:54 +0700

    Partial realization of ExecMgr

---
+++ src/system/exec/ExecMgr.h
@@ -0,0 +1,42 @@
+#pragma once 
+
+#include <thread>
+#include <mutex>
+
+#include <vector>
+#include <pair>
+#include <queue>
+
+#include "../types/AbstractCell.h"
+
+namespace ts {
+namespace system {
+  typedef std::pair<AbstractCell*, std::vector<AbstractCell*> > WorkCell;
+  class ExecMgr {
+  private:
+    ReduceData* reduceData;
+    std::queue<WorkCell> cellQueue;
+
+    System* system;
+
+    std::thread loopThread;
+    std::mutex queueMutex;
+    std::atomic<bool> end;
+
+  public:
+    ExecMgr(): reduceData(0), end(false) {}
+    ~ExecMgr() {}
+
+    void setSystem(System* _system) { system = _system; }
+
+    void run();
+    void join();
+
+    void add(const std::vector<WorkCell>& cells);
+
+  private:
+    void loop();
+    void compute(WorkCell& cell);
+  };
+}
+}
